8e70aa7d5d115999c18f07aee35bff6f40f57e43
unknown <luke.g@posteo.de>
2026-03-16 22:29:44 +0100

    LDR und BMP280 angeschlossen und Werte werden ausgelesen

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,30 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
+#include <WiFi.h>
 
 Adafruit_BMP280 bmp;
+
+//WiFi
+const char* ssid = "BLLLM";
+const char* password = "Schanzenshop100";
 
 //Intervalle
 unsigned long letzteMessung = 0;
 int messungWartezeit = 5000;
 
+//Pins
+const int ldrPin = 32; 
+
 void setup() {
+  WiFi.mode(WIFI_STA);
   Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.print("Verbinden");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 
   if (bmp.begin(0x76)) {
     Serial.println("Verbunden");
@@ -26,9 +41,12 @@ void loop() {
     letzteMessung = jetzt;
 
     Serial.print("Temperatur: ");
-    Serial.println(bmp.readTemperature());
+    Serial.print(bmp.readTemperature());
+    Serial.println("°C");
     Serial.print("Luftdruck: ");
     Serial.println(bmp.readPressure() / 100.0F);
+    Serial.print("Helligkeit: ");
+    Serial.println(analogRead(ldrPin));
   }
 
 
