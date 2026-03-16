9f81bcb7148ce0d1924de00dfa818d8697e06f4d
unknown <luke.g@posteo.de>
2026-03-17 00:10:49 +0100

    Webserver läuft und zeigt Werte an

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 #include <WiFi.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
 
+WebServer server(80);
 Adafruit_BMP280 bmp;
 
 //WiFi
@@ -17,25 +20,44 @@ int messungWartezeit = 5000;
 //Pins
 const int ldrPin = 32; 
 
+void startseiteSenden() {
+  float temp = bmp.readTemperature();
+  float press = bmp.readPressure() / 100;
+  float bright = analogRead(ldrPin);
+  String htmlSeite = "<h1>Umwelt-Monitor</h1>";
+  htmlSeite = htmlSeite + "<p>Temperatur: " + String(temp) + "°C</p> <p>Luftdruck: " + String(press) + "</p> <p>Helligkeit: " + String(bright) + "</p>";
+  server.send(200, "text/html", htmlSeite);
+}
+
 void setup() {
-  WiFi.mode(WIFI_STA);
   Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.print("Verbinden");
+  Serial.print("Mit WLAN verbinden");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+  Serial.println();
+
+  MDNS.begin("umweltmonitor");
 
   if (bmp.begin(0x76)) {
-    Serial.println("Verbunden");
+    Serial.println("BMP Verbunden");
   } else {
-    Serial.println("Fehlgeschlagen");
+    Serial.println("BMP Fehlgeschlagen");
   }
+
+  server.on("/", startseiteSenden);
+  server.begin();
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
   unsigned long jetzt = millis();
+
+  server.handleClient();
 
   if (jetzt - letzteMessung > messungWartezeit) {
     letzteMessung = jetzt;
